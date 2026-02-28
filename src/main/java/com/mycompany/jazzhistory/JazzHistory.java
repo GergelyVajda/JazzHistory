549fdebc629d61d5ee1096518b5b77f4b3a95973
GergelyVajda <51296438+GergelyVajda@users.noreply.github.com>
2026-02-28 19:19:31 +0100

    first commit

---
+++ src/main/java/com/mycompany/jazzhistory/JazzHistory.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jazzhistory;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author gvajd
+ */
+public class JazzHistory {
+
+    public static void main(String[] args) {
+        try {
+                FileReader buta = new FileReader("JazzHistory1.2.txt");
+            BufferedReader okos = new BufferedReader(buta);
+            
+            String beolvasas;
+            String ideiglenesSor = "";
+            Boolean irniKell =false;
+            String[] nyers = new String[5000];
+            int hossz1 = 0;
+            
+            for (int i = 0; i < 5000; i++) {
+                beolvasas=okos.readLine();
+                //System.out.println(beolvasas);
+                if (beolvasas==null) {
+                    //System.out.println(" ez a vége:"+hossz1);
+                    break;
+                }
+                if (beolvasas.contains("0") || beolvasas.contains("1") || beolvasas.contains("2") || beolvasas.contains("3") || beolvasas.contains("4")
+                        || beolvasas.contains("5") || beolvasas.contains("6") || beolvasas.contains("7") || beolvasas.contains("8") || beolvasas.contains("9") 
+                        || beolvasas.contains("LFZE") || beolvasas.contains("Friedrich") || beolvasas.contains("Károly") || !beolvasas.contains("(")|| beolvasas.contains(":")) 
+                {
+                    if (irniKell) {
+                        //System.out.println("1");
+                        nyers[hossz1]=ideiglenesSor;
+                        ideiglenesSor="";
+                        hossz1++;
+                        irniKell =false;
+                    }else{
+                        //System.out.println("2");
+                    i++;
+                     
+                    }
+                } else {
+                    if (irniKell) {
+                        //System.out.println("3");
+                        ideiglenesSor=ideiglenesSor.concat(beolvasas);
+                    }else{
+                        //System.out.println("4");
+                         ideiglenesSor=beolvasas;
+                        irniKell=true;
+                    }
+                }
+            }
+            
+            String [] sortores = new String[1000];
+            String[][] rendezett = new String[5000][2];
+            int hossz2=0;
+            String itiner = "[,.;()]";
+            for (int i = 0; i < 5000; i++){
+                if (nyers[i]==null) {
+                    break;
+                }
+               sortores=nyers[i].split(itiner);
+               for (String s : sortores) {
+	                System.out.println(s);
+                    }
+               
+                
+            }
+                         
+        } catch (FileNotFoundException ex) {
+            System.out.println("Hol a file?");
+                  
+        } catch (IOException ex) {
+            System.out.println("IOException, hiba az olvasásnál");
+        }
+    }
+}
